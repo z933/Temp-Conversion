55c8416894af75ff15dee469dcc9e6475819b758
z933 <142270708+z933@users.noreply.github.com>
2025-03-06 11:56:39 +1300

    Update test.py
    i changed the whole code to make the class function work

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
 import customtkinter
 
+customtkinter.set_appearance_mode("dark")
 
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("500x500")
+        self.title("Test")
 
-app = customtkinter.CTk()
+        
+        self.button = customtkinter.CTkButton(self, command=self.button_click)
+        self.button.grid(row=0, column=0, padx=20, pady=10)
+
+    
+    def button_click(self):
+        print("Button click")
+
+    
+
+app = App()
 app.mainloop()
